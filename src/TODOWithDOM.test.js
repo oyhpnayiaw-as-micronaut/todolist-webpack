cbe0e632e3c4b6c700cfbe564a4526436f64fbf5
EBrain <ebrain.ebrain@gmail.com>
2023-01-25 15:58:49 +0630

    Add unit test part 2

--- src/TODOWithDOM.test.js
+++ src/TODOWithDOM.test.js
@@ -43,4 +43,53 @@ describe('TODOWithDOM', () => {
     expect(todo.tasks.length).toBe(0);
     expect(todoListEl.children.length).toBe(0);
   });
+
+  test('should update a task description and render it in the DOM', () => {
+    // arrange
+    const description = 'test task';
+    todo.addTask(description);
+
+    // act
+    todo.updateTaskDesc(1, 'updated task');
+    todo.render();
+
+    // assert
+    expect(todo.tasks[0].description).toBe('updated task');
+    expect(todoListEl.children[0].querySelector('.todo-input').value).toBe(
+      'updated task',
+    );
+  });
+
+  test('should update a task status and render it in the DOM', () => {
+    // arrange
+    const description = 'test task';
+    todo.addTask(description);
+
+    // act
+    todo.updateTaskStatus(1);
+    todo.render();
+
+    // assert
+    expect(todo.tasks[0].completed).toBe(true);
+    expect(todoListEl.children[0].querySelector('.todo-checkbox').checked).toBe(
+      true,
+    );
+  });
+
+  test('should delete completed tasks from the list and render it in the DOM', () => {
+    // arrange
+    todo.addTask('test task 1');
+    todo.addTask('test task 2');
+    todo.updateTaskStatus(1);
+
+    // act
+    todo.deleteCompletedTasks();
+
+    // assert
+    expect(todo.tasks.length).toBe(1);
+    expect(todoListEl.children.length).toBe(1);
+    expect(todoListEl.children[0].querySelector('.todo-input').value).toBe(
+      'test task 2',
+    );
+  });
 });
